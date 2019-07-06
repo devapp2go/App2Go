bec39c8c98b9252becfe0080a7f6e25faddba60a
JonathanDelao <jonathan.delaoa@gmail.com>
2019-07-06 18:13:02 -0500

    Recommit

--- App2GoClientWeb/ClientApp/src/App.js
+++ App2GoClientWeb/ClientApp/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Route } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Counter from "./components/Counter";
 import FetchData from "./components/FetchData";
 import Principal from "./pages/Principal";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default () => (
   <Layout>
+    <
     <Route exact path="/" component={Principal} />
     <Route path="/counter" component={Counter} />
     <Route path="/fetch-data/:startDateIndex?" component={FetchData} />
