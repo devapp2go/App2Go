70f406b8cedd1be71959312d69006a979e8dd4de
Diego <diego.partida@mtsoftech.com>
2019-07-05 22:55:26 -0500

    login

---
+++ App2GoAdminWeb/ClientApp/src/components/Login.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import './login.css';
+import { Input, Button } from 'reactstrap';
+
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <div class="greeting">
+        <p>¡Hola! Bienvenido a</p>
+        <img class="logo" src={process.env.PUBLIC_URL + "/media/img/LOGO.png"}></img>
+      </div>
+    );
+  }
+}
+
+class Oauth extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Inicia sesión con</p>
+        <div class="oauth-fb"></div>
+        <br></br>
+        <div class="oauth-google"></div>
+        <p>o</p>
+      </div>
+    );
+  }
+}
+
+class LoginData extends React.Component {
+  render() {
+    return (
+      <div>
+        <input class="input-login" placeholder = "Correo electrónico"></input>
+        <br></br>
+        <input class="input-login" placeholder = "Contraseña" type="password"></input>
+        <small>¿Olvidaste tu contraseña? </small><a class="a-login" href="#">Haz click aquí</a>
+        <div class="div-button">
+        <button>INICIAR SESIÓN</button>
+        </div>
+        <hr class="hrlogin"></hr>
+      </div>
+    );
+  }
+}
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <small>¿Aún no tienes una App?</small>
+        <div class="div-button">
+        <button>¡CREA TU APP!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div class="text background">
+          <Greeting/>
+          <Oauth/>
+          <LoginData/>
+          <Register/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Login />,
+  document.getElementById('root')
+);
+export default connect()(Login);
